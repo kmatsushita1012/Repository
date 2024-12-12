5c6dbc72b03b2b4725a56188d2bc215acc8a0c23
kmatsushita1012 <kmatsushita1012@icloud.com>
2024-12-12 22:18:18 +0900

    12122218

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,15 @@ import 'package:repository/providers/repository_provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:repository/providers/settings_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp(
     prefs: prefs,
   ));
