9a9f3a80c340c240bd80dfafacb9b5719c3bcfe3
kmatsushita1012 <kmatsushita1012@icloud.com>
2024-12-16 20:50:28 +0900

    FIx analyze conflict

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
